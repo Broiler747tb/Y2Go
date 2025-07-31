c2b2a5244c0dff603884a182debf24d8fd9b0be7
Daniel Kosovskiy <churkabes420@gmail.com>
2025-08-01 00:50:36 +0300

    Remade the whole app, now supports: Mp3, Flac, Vorbis, Wav

--- go.mod
+++ go.mod
@@ -3,13 +3,22 @@ module Y2Go
 go 1.24.4
 
 require (
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
 	github.com/dhowden/tag v0.0.0-20240417053706-3d75831295e8
-	github.com/hajimehoshi/oto v1.0.1
-	github.com/tosone/minimp3 v1.0.2
+	github.com/faiface/beep v1.1.0
 )
 
 require (
-	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
+	github.com/hajimehoshi/oto v1.0.1 // indirect
+	github.com/icza/bitio v1.1.0 // indirect
+	github.com/jfreymuth/oggvorbis v1.0.1 // indirect
+	github.com/jfreymuth/vorbis v1.0.0 // indirect
+	github.com/mewkiz/flac v1.0.13 // indirect
+	github.com/mewkiz/pkg v0.0.0-20250417130911-3f050ff8c56d // indirect
+	github.com/mewpkg/term v0.0.0-20241026122259-37a80af23985 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/exp/shiny v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/image v0.24.0 // indirect
 	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
 	golang.org/x/sys v0.34.0 // indirect
--- main.go
+++ main.go
@@ -1,84 +1,29 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"github.com/dhowden/tag"
-	"github.com/hajimehoshi/oto"
-	"github.com/tosone/minimp3"
-	"image"
-	"image/jpeg"
-	"image/png"
-	"log"
-	"os"
-	"time"
+	"Y2Go/Cli"
+	"Y2Go/Flac"
+	"Y2Go/Mp3"
+	"Y2Go/Vorbis"
+	"Y2Go/Wav"
+	"strings"
 )
 
-// /home/daniil/Downloads/OldFlavours.mp3
-
 func main() {
-	path := "/home/daniil/Downloads/OldFlavours.mp3"
+	path := CLI.GreeterAndSelecter()
+	var data *CLI.Metadata
 
-	var err error
-	var file []byte
-	if file, err = os.ReadFile(path); err != nil {
-		log.Fatal(err)
-	} // Scanning the file for minimp3
-
-	printMetadata(path) // Reading the metadata from a file and printing it
-
-	var dec *minimp3.Decoder
-	var data []byte
-	if dec, data, err = minimp3.DecodeFull(file); err != nil {
-		log.Fatal(err)
-	} // Decoding the scanned mp3 file
-
-	//leng := songLength(dec, data)
-	//go lenghtConverter(leng)
-
-	var context *oto.Context
-	if context, err = oto.NewContext(dec.SampleRate, dec.Channels, 2, 256); err != nil {
-		log.Fatal(err)
-	} // I honestly dont know what is ts
-
-	var player = context.NewPlayer()
-	player.Write(data) // Playing the decoded thing
-
-	<-time.After(time.Second)
-
-	dec.Close() // Player ending and after 1 second closing the decoder
-	if err = player.Close(); err != nil {
-		log.Fatal(err)
+	if strings.HasSuffix(path, ".mp3") {
+		Mp3.PlayMp3(path)
 	}
-}
-
-func printMetadata(path string) {
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
+	if strings.HasSuffix(path, ".flac") {
+		Flac.PlayFlac(path)
 	}
-	defer file.Close()
-
-	metadata, err := tag.ReadFrom(file)
-	if err != nil {
-		log.Fatal(err)
+	if strings.HasSuffix(path, ".wav") {
+		Wav.PlayWav(path)
 	}
-	pic := metadata.Picture()
-	if pic != nil {
-		decodeImage(pic.Data, pic.MIMEType)
+	if strings.HasSuffix(path, ".ogg") {
+		Vorbis.PlayVorbis(path)
 	}
-	fmt.Sprintf(metadata.Title())
-	fmt.Sprintf("By %v", metadata.Artist())
-}
-
-func decodeImage(data []byte, mimeType string) (image.Image, error) {
-	reader := bytes.NewReader(data)
-	switch mimeType {
-	case "image/jpeg", "image/jpg":
-		return jpeg.Decode(reader)
-	case "image/png":
-		return png.Decode(reader)
-	default:
-		return nil, fmt.Errorf("unsupported image format: %s", mimeType)
-	}
+	CLI.PrintMetadata(*data)
 }
